46b963e2bfc59b73bb2aad99cbc85b2ba4b4a941
nguyenztruong2306-coder <nguyenztruong2306@gmail.com>
2026-03-20 10:01:25 +0700

    Update full project snapshot

--- .tmp_build/_debug_export_check.sql
+++
@@ -1,3 +0,0 @@
-SELECT sp.masp, sp.tensp, sp.soluongton FROM sanpham sp WHERE sp.tensp LIKE '%iPhone 13%';
-SELECT pb.maphienbansp, pb.masp, pb.soluongton FROM phienbansanpham pb WHERE pb.masp IN (SELECT masp FROM sanpham WHERE tensp LIKE '%iPhone 13%');
-SELECT imei, product_id, variant_id, sold, import_receipt_id, export_receipt_id FROM imei_registry WHERE product_id IN (SELECT masp FROM sanpham WHERE tensp LIKE '%iPhone 13%') ORDER BY created_at DESC LIMIT 20;
